42a960524af8137ac6ef6820f21c3925c17726fd
Myrel666 <Myrel666@users.github.com>
2021-12-04 22:41:30 +0700

    commit home ui

---
+++ ScanMe/app/src/main/java/com/example/scanme/ui/artikel/ArtikelDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.scanme.ui.artikel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.scanme.R
+
+class ArtikelDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artikel_detail)
+    }
+}
